8f66d11953e957cff4d5338d63dfd12bd9116dd1
SAHID HOSSAIN <sahid2518@student.nstu.edu.bd>
2026-05-23 00:00:04 +0600

    another problem solved today

---
+++ DIV 3/1096_B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        string s;
+
+        cin >> n >> s;
+
+        int open = 0, close = 0;
+
+        for (char c : s) {
+            if (c == '(')
+                open++;
+            else
+                close++;
+        }
+
+        if (open == close)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
