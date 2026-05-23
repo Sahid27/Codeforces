4d00c3daea127595b7624fc6374640016a992ca4
SAHID HOSSAIN <sahid2518@student.nstu.edu.bd>
2026-05-23 21:34:27 +0600

    I like this type of easy problem .. ha ha

---
+++ 1829A_Love story.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    string target = "codeforces";
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (s[i] != target[i]) {
+                count++;
+            }
+        }
+
+        cout << count << "\n";
+    }
+
+    return 0;
+}
